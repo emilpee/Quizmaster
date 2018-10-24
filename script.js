1c315e040cda50864b070a032f913102a67f8631
Emil Petersson <emilpet93@gmail.com>
2018-10-24 14:16:28 +0200

    Lite nya uppdateringar

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+var response;
 
 document.addEventListener('DOMContentLoaded', function() {
 var content = document.getElementById("content");
 var httpReq = new XMLHttpRequest();
-var response;
   
 httpReq.onreadystatechange = function() {
     if (httpReq.readyState == 4) {
@@ -16,23 +16,65 @@ httpReq.onreadystatechange = function() {
                 quiz.id = index;
                 content.appendChild(quiz); 
                 var quizContent = ` 
-                  <div class="quizContent">
+                  <div id="quizContent">
                   <h2> ${question.question} </h2>
-                  <button type="button" class="btn" id="trueButton">True</button>
-                  <button type="button" class="btn" id="falseButton">False</button>
+                  <p class="correct"><strong>Correct answer: ${question.correct_answer} </strong></p>
+                  <input type="button" class="btn" value="True" id="trueButton">
+                  <input type="button" class="btn" value="False" id="falseButton">
                 </div> `;
                document.getElementById(quiz.id).innerHTML = quizContent;
             });
 
+    var btn = document.getElementsByClassName('btn'); 
+    var printCorrect = document.getElementsByTagName('p'); 
+    var trueButton = document.getElementById('trueButton');
+    // var falseButton = document.getElementById('falseButton');
+    quizContent = document.getElementById('quizContent');
+    var numOfQuestions = response.results.length;
+    
+    var answers = []; // array som lagrar användarens svar
+    var currentQ = 0; // visar nuvarande fråga
 
-        } else {
-            console.log("HTTP-status är inte 200, utan" + httpReq.status);
-        }
+    // Lagra alla korrekta svar
+    var correctAnswer = response.results[currentQ].correct_answer;
+    for (var i = 0; i <= 9; i++) {
+      console.log(correctAnswer);
+      correctAnswer = response.results[i].correct_answer;
+
+
+
+    if (btn === correctAnswer) {
+        alert("Correct!");
+        correctAnswer++;
+        currentQ++;
+    } else {
+        currentQ++;
     }
+
+    }
+
+    // if (true == correct_answer) {answers.push();} 
+    // else if (true == incorrect_answers) {do not push.}
+    content.addEventListener('click', function(e) {
+        if (btn) {
+            for (var x = 0; x < printCorrect.length; x++) {
+              printCorrect[x].classList.remove('correct');
+            }
+        } 
+    }); 
+
+    // Lägg till knapp som visar resultat
+    content.appendChild(submitBtn);
+    submitBtn.addEventListener('click', function() {
+        alert(correctAnswer + ' out of ' + numOfQuestions);
+    });
+}
+}
 };
+
 
 httpReq.open('GET', 'https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=boolean');
 httpReq.responseType = 'json';
 httpReq.send();
-
+console.log(httpReq);
 });
